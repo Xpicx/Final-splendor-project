876358aa5a1566912b2b9852401046af1382e97a
Phuong-Vy <159920161+Phuong-Vy@users.noreply.github.com>
2024-12-15 21:57:40 +0100

    Update BuyCardAction.java

--- BuyCardAction.java
+++ BuyCardAction.java
@@ -6,7 +6,8 @@ public class BuyCardAction implements Action
     public BuyCardAction(DevCard carte){
         this.carte=carte;
     }
-    
+
+    //Retire une carte du board pour la mettre dans dans le paquet du joueur et retire les resources avec lesquelles le joueur a acheté la carte 
     public void process(Player joueur,Board plateau){
         plateau.updateCard(carte);
         joueur.addPurchasedCard(carte);
@@ -16,7 +17,8 @@ public class BuyCardAction implements Action
         }
         joueur.updatePoints(carte.getPoints());
     }
-    
+
+    //Renvoie la carte que le joueur a acheté
     public String toString(){
         return "Vous avez acheté "+carte+" !";
     }
